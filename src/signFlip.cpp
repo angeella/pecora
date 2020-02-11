c1bdf3c0c1fd25a05fa7201b8848db5be5a4081d
angeella <a.andreella8@gmail.com>
2020-02-11 11:02:40 +0100

    update

--- src/signFlip.cpp
+++ src/signFlip.cpp
@@ -11,18 +11,22 @@ arma::mat signFlip(arma::mat X, double B) {
   
   arma::mat T(m, B, arma::fill::zeros);
   
-  arma::vec eps, Tb, id, Tb2, Tbp, Ts;
+  arma::vec eps, M, id, Tb2, V, T0, Tb;
   
   int bb;
   for (bb=0; bb<B; bb++) {
     eps = Rcpp::rbinom(n, 1, 0.5)*2 - 1;  // signs
     id = Rcpp::rbinom(n, 1, 1)*2 - 1; //identity
     Tb = X * eps;
-    Tb = Tb /n; //mean
-    Tb2 = pow (Tb,2)/pow (n,2); //mean power 2
-    Tbp = (pow (X,2) * id)/n; //power 2 mean
-    Ts = Tb/ pow ((Tbp-Tb2)*n/(n-1), 0.5); //final test
-    T.col(bb) = Ts;
+    M = Tb /n; //mean
+    //    Tb2 = pow (Tb,2)/pow (n,2); //mean power 2
+    //    Tbp = (pow (X,2) * id)/n; //power 2 mean
+    Tb2 = (pow(X, 2) * id)/(n-1); //mean x^2
+    //Tb0 = Tb0/n;
+    V = (Tb2 - (pow(Tb,2))/(n*(n-1)));
+    //    Ts = Tb/ pow (((Tbp-Tb2)*n)/(n-1), 0.5); //final test
+    T0 = M/ pow((V)/n,0.5);
+    T.col(bb) = T0;
     
   }
   return (T);
