38138e0300bb2ac05c61b98623c7bac18136a5cc
Angela Andreella <36375295+angeella@users.noreply.github.com>
2026-06-15 15:42:51 +0200

    solved bug permTest

--- R/twoSamples.R
+++ R/twoSamples.R
@@ -2,7 +2,7 @@
 #' @description Performs two-sample t-tests by permutations.
 #' @usage twoSamples(X, B = 1000, seed = 1234, 
 #' permReturn = TRUE, label = NULL)
-#' @param X data matrix where columns represent the \code{m} variables and rows the \code{n} observations. The columns' name defines the groups' label.
+#' @param X data matrix where columns represent the \code{m} variables and rows the \code{n} observations. The rows's name defines the groups' label.
 #' @param B numeric value, number of permutations to be performed, including the identity. Default is 1000.
 #' @param seed numeric value, specify seed. Default is 1234.
 #' @param permReturn logical value, \code{TRUE} to return the t-tests and p-values permutation distribution. Default is \code{TRUE}.
@@ -51,10 +51,10 @@ twoSamples <- function(X, B = 1000,
   id <- levels(label)
   n1 <- sum(label==id[1])
   n2 <- sum(label==id[2])
-  colV1 <- colVariances(X[,label == id[1]])
-  colV2 <-colVariances(X[,label == id[2]])
-  colM1 <- colMeans(X[,label == id[1]])
-  colM2 <-colMeans(X[,label == id[2]])
+  colV1 <- colVariances(X[label == id[1],])
+  colV2 <-colVariances(X[label == id[2],])
+  colM1 <- colMeans(X[label == id[1],])
+  colM2 <-colMeans(X[label == id[2],])
   pooled.var <- (colV1/n1 + colV2/n2)
   Test <- (colM1 - colM2)/sqrt(pooled.var)
   Test <- ifelse(is.na(Test), 0 , Test)
